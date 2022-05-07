d8fd3688519c467c46ca8bb7466e07665f69b6e2
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-07 15:07:20 +0800

    action.bound,click处不用函数形式

--- mobx6-demo/src/Components/FucComponentsDemo.tsx
+++ mobx6-demo/src/Components/FucComponentsDemo.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { observer } from 'mobx-react'
-import counter from '../store/Counter'
+import React from "react";
+import { observer } from "mobx-react";
+import counter from "../store/Counter";
 
 const FucComponentsDemo = () => {
   return (
     <div>
-       <h3>计数器</h3>
+      <h3>计数器</h3>
       <div>点击次数:{counter.counter}</div>
       <div>
-        <button onClick={()=>counter.increment()}>+1</button>
-        <button onClick={()=>counter.resset()}>restart</button>
+        <button onClick={() => counter.increment()}>+1</button>
+        <button onClick={() => counter.resset()}>restart</button>
+        <button
+          onClick={counter.decrement}
+        >{`-1（不用()=>的写法，store下属性加bound）`}</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default observer(FucComponentsDemo)
+export default observer(FucComponentsDemo);
--- mobx6-demo/src/store/Counter.ts
+++ mobx6-demo/src/store/Counter.ts
@@ -1,22 +1,25 @@
-import { action, makeObservable, observable } from "mobx"
-
+import { action, makeObservable, observable } from "mobx";
 
 class Counter {
-    counter = 0
-    constructor(){
-        //参数1： 指定谁变成响应式
-        //参数2：指定哪些属性或者方法变成可观察的
-        makeObservable(this,{
-            counter: observable, //counter是可观察的
-            increment: action, //修改counter的方法是个action
-            resset: action
-        })
-    }
-    increment() {
-        this.counter++
-    }
-    resset(){
-        this.counter = 0
-    }
+  counter = 0;
+  constructor() {
+    //参数1： 指定谁变成响应式
+    //参数2：指定哪些属性或者方法变成可观察的
+    makeObservable(this, {
+      counter: observable, //counter是可观察的
+      increment: action, //修改counter的方法是个action
+      resset: action,
+      decrement: action.bound,
+    });
+  }
+  increment() {
+    this.counter++;
+  }
+  resset() {
+    this.counter = 0;
+  }
+  decrement() {
+    this.counter--;
+  }
 }
-export default new Counter()
+export default new Counter();
