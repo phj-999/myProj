ad683777936ad4393295091c0dd8bfae02ffc37e
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-10 23:53:18 +0800

    router

--- r-mobx/src/App.js
+++ r-mobx/src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>About</div>} />
+      </Routes>
     </div>
   );
 }
