e6869fd819201e312b889b81b2f05098d5ab1eef
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-17 21:02:54 +0800

    react-three-fiber构建

--- thr-fiber-int/src/App.js
+++ thr-fiber-int/src/App.js
@@ -1,10 +1,18 @@
+import { Canvas } from "react-three-fiber";
+
 import "./App.css";
-import BoxOne from "./views/box1demo";
+//import BoxOne from "./views/box1demo";
+import Box from "./components/Box";
 
 function App() {
   return (
     <div className="App">
-      <BoxOne />
+      {/* <BoxOne /> */}
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <Canvas style={{ background: "red" }}>
+          <Box />
+        </Canvas>
+      </div>
     </div>
   );
 }
---
+++ thr-fiber-int/src/components/Box.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import { useFrame } from "react-three-fiber";
+/**
+ *state中是如下api
+ *advance, camera, clock, events, get, gl, internal
+ *invalidate, mouse, onPointerMissed, performance
+ * raycaster, scene, set, setEvents, setFrameloop
+ * viewport, xr,  pointer, setSize
+ */
+const Box = () => {
+  const ref = useRef();
+  useFrame((state) => {
+    //   console.log(state);
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color={"blue"} />
+    </mesh>
+  );
+};
+
+export default Box;
