19cf7dab50217e3578b6899d62531c0f1a7e19fb
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-19 14:46:35 +0800

    style: Model

--- thr-fiber-int/src/components/boxTwo/Model.jsx
+++ thr-fiber-int/src/components/boxTwo/Model.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useLoader } from "@react-three/fiber";
-import * as THREE from "three";
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
 const Model = (props) => {
