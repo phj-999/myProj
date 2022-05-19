1f1459db96938429be1833f56a626b197b472009
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-19 23:49:10 +0800

    格式化风格

--- thr-fiber-int/src/components/boxTwo/CameraButton.jsx
+++ thr-fiber-int/src/components/boxTwo/CameraButton.jsx
@@ -3,55 +3,53 @@
 import React from "react";
 import state from "../../state";
 
-const style ={
-    position: "absolute",
-    buttom: "30vh",
-    textAlign:"center",
-    height:'30px',
-    width: '30px',
-    borderRadius: '100%',
-    fontSize: 20,
-    background: 'white',
-    fontWeight: 'bold',
-    opacity: 0.7,
-    border: '1px solid black',
-    zIndex: 1,
-}
+const style = {
+  position: "absolute",
+  buttom: "30vh",
+  textAlign: "center",
+  height: "30px",
+  width: "30px",
+  borderRadius: "100%",
+  fontSize: 20,
+  background: "white",
+  fontWeight: "bold",
+  opacity: 0.7,
+  border: "1px solid black",
+  zIndex: 1,
+};
 
 const CameraButton = () => {
-    const sets = {
-        1: {
-            cameraPos:[0,-5,4],
-            target: [4,0,0]
-        },
-        2: {
-            cameraPos: [1,2,5],
-            target: [-4,0,0]
-        }
-    }
-    const handleClick =(num) => {
-state.cameraPos.set(...sets[num].cameraPos)
-state.cameraPos.set(...sets[num].target)
-state.shouldUpdate = true
-    }
+  const sets = {
+    1: {
+      cameraPos: [0, -5, 4],
+      target: [4, 0, 0],
+    },
+    2: {
+      cameraPos: [1, 2, 5],
+      target: [-4, 0, 0],
+    },
+  };
+  const handleClick = (num) => {
+    state.cameraPos.set(...sets[num].cameraPos);
+    state.cameraPos.set(...sets[num].target);
+    state.shouldUpdate = true;
+  };
   return (
     <>
       <button
-      onClick={e=>handleClick(2)}
+        onClick={(e) => handleClick(2)}
         style={{
-            left: "40vw",
-          ...style
-
+          left: "40vw",
+          ...style,
         }}
       >
         {"<"}
       </button>
       <button
-       onClick={e=>handleClick(1)}
+        onClick={(e) => handleClick(1)}
         style={{
           right: "40vw",
-        ...style
-
+          ...style,
         }}
       >
         {">"}
