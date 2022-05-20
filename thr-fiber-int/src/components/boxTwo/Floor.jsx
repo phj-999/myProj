1d6798aaf06b9ef0e3d0e16f5bf483539ed818ff
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-20 13:36:55 +0800

    给地板加上纹理

--- thr-fiber-int/src/components/boxTwo/Floor.jsx
+++ thr-fiber-int/src/components/boxTwo/Floor.jsx
@@ -2,13 +2,17 @@
 
 import React from 'react'
 import { useBox } from '@react-three/cannon'
+import { useLoader } from '@react-three/fiber'
+import * as THREE from 'three'
 
 const Floor = props => {
   const [ref,api] = useBox(()=>({args:[20,1,10], ...props}))
+  const texture = useLoader(THREE.TextureLoader, '/threebox/wood.jpg')
+
   return (
     <mesh {...props} receiveshadow ref={ref} >
         <boxBufferGeometry args={[50,0.7,50]}/>
-        <meshPhysicalMaterial  color={'black'} transparent opacity={1} />
+        <meshPhysicalMaterial transparent opacity={0.5} map={texture} />
     </mesh>
   )
 }
