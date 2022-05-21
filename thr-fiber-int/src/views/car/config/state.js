c30767c3112a714f3dfaa423e5c66268adfccea3
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-22 00:00:10 +0800

    eidt state

--- thr-fiber-int/src/views/car/config/state.js
+++ thr-fiber-int/src/views/car/config/state.js
@@ -5,7 +5,7 @@ const state = {
   activeMeshName: 'object001_Material009_0',
   cameraPos: new THREE.Vector3(7, 7, 7), //也可以用new THREE.Vector3的形式写
   target: new THREE.Vector3(4, 0, 0),
-  shouldUpdate: false,
+  shouldUpdate: false
 };
 
 export default state;
