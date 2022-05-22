fd80c1ba6fc89fdf01a316bfb230f23a049348cf
coderbeliver <77000489+phj-999@users.noreply.github.com>
2022-05-22 19:09:00 +0800

    add: 灯光； edit: 线条几合体、lambert几何体坐标和颜色

--- thr-fiber-int/src/views/start/index.jsx
+++ thr-fiber-int/src/views/start/index.jsx
@@ -8,9 +8,10 @@ const Start = () => {
   //const Scene =  new THREE.Scene() 这样每次都会实例化一次 重复渲染浪费性能
   const Scene = useRef(new THREE.Scene()).current; //场景
   const Camera = useRef(new THREE.PerspectiveCamera()).current; //相机
-  const Render = useRef(new THREE.WebGL1Renderer({ actialias: true })).current; // 渲染器
+  const Render = useRef(new THREE.WebGLRenderer({ actialias: true })).current; // 渲染器
   const Meshs = useRef([]).current; // 物体
   const id = useRef(null);
+  const Lights = useRef([]).current;
   /**
    * 创建网格模型
    */
@@ -21,12 +22,12 @@ const Start = () => {
     const mashBasicMaster = new THREE.MeshBasicMaterial({ color: "red" });
     // 网格
     const mesh = new THREE.Mesh(react, mashBasicMaster);
-    mesh.position.set(0, -5, 0);
+    mesh.position.set(0, 0, 0);
     Scene.add(mesh); ////网格模型添加到场景中
     Meshs.push(mesh); // 方便调用
   }, [Scene]);
 
-  /**创建线条 */
+  /**创建线条几何体 */
   const createLine = useCallback(() => {
     // 线条材质
     const lineMater = new THREE.LineBasicMaterial({
@@ -36,58 +37,61 @@ const Start = () => {
     const geomatry = new THREE.BufferGeometry();
     const color = new THREE.Color();
 
-      let vertices = [];
-      let color1 = [];
+    let vertices = [];
+    let color1 = [];
 
     for (let i = 0; i < 8000; i++) {
       const x = Math.random() * 2 - 1;
       const y = Math.random() * 2 - 1;
       const z = Math.random() * 2 - 1;
       //geomatry.vertics.push(new THREE.Vector3(x, y, z));
-      vertices.push(x,y,z)
+      vertices.push(x, y, z);
       //geomatry.colors.push(Math.random(), Math.random(), Math.random());
       color.setHSL(Math.random(), Math.random(), Math.random());
-      color1.push(color.r,color.g,color.b)
+      color1.push(color.r, color.g, color.b);
     }
     //新版
 
-     geomatry.setFromPoints(vertices);
+    geomatry.setFromPoints(vertices);
 
     geomatry.setAttribute(
-
-      'position',
+      "position",
 
       new THREE.Float32BufferAttribute(vertices, 3)
-
     );
 
-    geomatry.setAttribute('color', new THREE.Float32BufferAttribute(color1, 3));
-    const mesh = new THREE.Line(geomatry,lineMater)
+    geomatry.setAttribute("color", new THREE.Float32BufferAttribute(color1, 3));
+    const mesh = new THREE.Line(geomatry, lineMater);
+    mesh.position.set(4, 0, 0);
     Scene.add(mesh);
     Meshs.push(mesh);
   }, []);
 
   /**创建lambert材质立方体 */
-  const createLambert = useCallback(
-    () => {
-      //几何体
-    const react = new THREE.BoxBufferGeometry(2,2,2);
+  const createLambert = useCallback(() => {
+    //几何体
+    const react = new THREE.BoxBufferGeometry(2, 2, 2);
     //材质
-    const lambert = new THREE.MeshLambertMaterial({ 
-      color: 0x0000ff,
+    const lambert = new THREE.MeshLambertMaterial({
+      color: "white",
       side: THREE.DoubleSide, //两面可见
     });
     //网格
     const mesh = new THREE.Mesh(react, lambert);
-    var a = new THREE.Vector3(-4, 5, 0);
-    mesh.position.set(a);
+    //var a = new THREE.Vector3(-4, 0, 0);
+    mesh.position.set(-4, 0, 0);
     Scene.add(mesh);
     Meshs.push(mesh);
     // console.log(32323);
-    },
-    [Scene],
-  )
+  }, [Scene]);
 
+  /**灯光 */
+  const createLight = () => {
+    const dirLight = new THREE.DirectionalLight("#48dbfb", 1);
+    dirLight.position.set(100, 100, 100);
+    Scene.add(dirLight);
+    Lights.push(dirLight);
+  };
 
   const init = useCallback(() => {
     //渲染器尺寸
@@ -131,6 +135,7 @@ const Start = () => {
   useEffect(() => {
     Body.current.append(Render.domElement); //body元素中插入canvas对象
     init();
+    createLight(); //灯光
     creatReact();
     createLine(); //创建线条
     createLambert(); //创建第二个盒子
@@ -144,6 +149,13 @@ const Start = () => {
         item.geometry.dispose(); //废弃几何体
         item.material.dispose(); //废弃材质
       });
+      Lights.forEach((item) => {
+        Scene.remove(item);
+        item.dispose();
+        console.log("lights 销毁");
+      });
+      Render.dispose();
+      //Scene.dispose()
     };
   }, []);
 
